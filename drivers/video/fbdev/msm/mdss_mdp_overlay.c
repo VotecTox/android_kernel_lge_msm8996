b3d28ac11ca8536ad0c4071c118fbb8922ad8bbe
Adrian Salido-Moreno <adrianm@codeaurora.org>
2015-10-05 17:48:03 -0700

    msm: mdss: properly clean up pipe in case of failure during reuse
    If there is a pipe failure during reuse, the pipe may still have
    pending buffers that need to be cleaned up. By going through pipe
    destroy function directly we may be bypassing this, so go through
    proper cleanup path instead.
    
    Change-Id: Ib63e3c5679f91a8642e2770e94ff08f046d3a913
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_overlay.c
+++ drivers/video/fbdev/msm/mdss_mdp_overlay.c
@@ -78,6 +78,8 @@ extern void lcd_watch_font_crc_check(struct msm_fb_data_type * mfd);
 static int mdss_mdp_overlay_free_fb_pipe(struct msm_fb_data_type *mfd);
 static int mdss_mdp_overlay_fb_parse_dt(struct msm_fb_data_type *mfd);
 static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd);
+static void __overlay_pipe_cleanup(struct msm_fb_data_type *mfd,
+		struct mdss_mdp_pipe *pipe);
 static void __overlay_kickoff_requeue(struct msm_fb_data_type *mfd);
 static void __vsync_retire_signal(struct msm_fb_data_type *mfd, int val);
 static int __vsync_set_vsync_handler(struct msm_fb_data_type *mfd);
@@ -1124,7 +1126,7 @@ exit_fail:
 		pr_debug("failed for pipe %d\n", pipe->num);
 		if (!list_empty(&pipe->list))
 			list_del_init(&pipe->list);
-		mdss_mdp_pipe_destroy(pipe);
+		__overlay_pipe_cleanup(mfd, pipe);
 	}
 
 	/* invalidate any overlays in this framebuffer after failure */
